07fbff2a5983201dfab5b061639876f2917a4a47
qiyu <qiyu56@qq.com>
2021-09-30 00:40:26 +0800

    office doc coding

---
+++ greetings/greetings.go
@@ -0,0 +1,49 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	// In this for loop, range returns two values:
+	// the index of the current item in the loop and a copy of the item's value.
+	// You don't need the index, so you use the Go blank identifier (an underscore) to ignore it.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+// Go executes init functions automatically at program startup,
+// after global variables have been initialized.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
