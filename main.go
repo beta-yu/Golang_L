b25414476558355ba800e079356ec190b79914dc
qiyu <qiyu@tantanapp.com>
2021-09-29 18:34:06 +0800

    rpc

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println(get(2))
+	// fmt.Println("Hello world")
+	// fmt.Println(get(2))
+	stu := Student{
+		name: "Tom",
+	}
+	fmt.Println(stu.hello("Jack"))
 }
 
 func get(index int) int {
 	arr := []int{2, 3, 4}
 	return arr[index]
+}
+
+type Student struct {
+	name string
+}
+
+func (stu *Student) hello(person string) string {
+	return fmt.Sprintf("hello %s, I am %s.", person, stu.name)
 }
